66ab53ee26aa0d7cb9c5af29433d845147d02386
amanatulhay <amanatulhay.pribadi@gmail.com>
2024-08-20 17:35:04 +0800

    add vercel setup

--- askSANBER/resources/views/layouts/master.blade.php
+++ askSANBER/resources/views/layouts/master.blade.php
@@ -13,8 +13,6 @@
   <link rel="stylesheet" href="{{asset("/template/dist/css/adminlte.min.css")}}">
 
   @stack('styles')
-  <!-- TinyMCE -->
-  <script src="https://cdn.tiny.cloud/1/agmjhm519kta29qh5mxzlp7ytq1eais8mqji3pw37xgzus5b/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
 
 </head>
 <body class="hold-transition sidebar-mini">
@@ -101,22 +99,6 @@
 <script src="{{asset("/template/plugins/bootstrap/js/bootstrap.bundle.min.js")}}"></script>
 <!-- AdminLTE App -->
 <script src="{{asset("/template/dist/js/adminlte.min.js")}}"></script>
-
-<!-- TinyMCE -->
-  <script>
-    tinymce.init({
-        selector: 'textarea',
-        plugins: 'ai tinycomments mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss',
-        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
-        tinycomments_mode: 'embedded',
-        tinycomments_author: 'Author name',
-        mergetags_list: [
-        { value: 'First.Name', title: 'First Name' },
-        { value: 'Email', title: 'Email' },
-        ],
-        ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
-    });
-    </script>
 
 @stack('scripts')
 @include('sweetalert::alert')
